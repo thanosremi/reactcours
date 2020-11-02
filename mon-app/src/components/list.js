0c3498460330905f9e5f70d173993419b13a9f95
thanosremi <73822234+thanosremi@users.noreply.github.com>
2020-11-02 16:42:16 +0100

    add list component

---
+++ mon-app/src/components/list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class List extends React.Component{
+
+    render(){
+        return(
+        <div>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagrame</li>
+                <li>Monster</li>
+            </ul>
+        </div>
+        );
+    }
+}
